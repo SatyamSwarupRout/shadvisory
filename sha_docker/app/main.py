526de4ebe11f934a7d5dc07036b9f60ecc3dd5ab
Sukanta Nanda <sukantan@addyaa.com>
2025-10-18 15:18:02 +0530

    Refactor Sahayak endpoints with Pydantic models
    Refactor Sahayak endpoints to use Pydantic models for data validation and serialization.

--- sha_docker/app/main.py
+++ sha_docker/app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from . import models, database
+from . import models, database, pydantic_classes 
 from sqlalchemy.ext.declarative import declarative_base
+from typing import List
+
+
 
 Base = declarative_base()
 engine = database.engine
@@ -42,40 +45,39 @@ def get_sahayaks(db: Session = Depends(database.get_db)):
 def get_sahayak(phone: str, db: Session = Depends(database.get_db)):
     try:
         sahayak = db.query(models.Sahayak).filter(models.Sahayak.phone_number == phone).first()
-        return sahayak
+        sahayak_base = pydantic_classes.SahayakBase(
+            phone_number=sahayak.phone_number,
+            email=sahayak.email,
+            first_name=sahayak.first_name,
+            last_name=sahayak.last_name,
+            aadhaar_number=sahayak.aadhaar_number,
+            village=sahayak.village,
+            block_name=sahayak.block_name,
+            district=sahayak.district,
+            state=sahayak.state,
+            gender_category=sahayak.gender_category,
+            education_level=sahayak.education_level,
+            is_active=sahayak.is_active,
+            is_approved=sahayak.is_approved,
+            pincode=sahayak.pincode
+        )
+        return sahayak_base
     except Exception as e:
         print("❌ Error occurred while fetching Sahayak:", e)
         return {"error": "Failed to fetch Sahayak"}
 
 
 @app.post("/register_sahayak")
-def add_sahayak(phone_number: str, 
-                email: str, 
-                first_name: str, 
-                last_name: str, 
-                aadhaar_number: str,
-                village: str, 
-                block_name: str, 
-                district: str,
-                state: str,
-                gender_category: str,
-                education_level: str,
-                is_active: bool,
-                is_approved: bool,
-                pincode: int,
-                db: Session = Depends(database.get_db)):
+def add_sahayak(sahayak: pydantic_classes.SahayakBase, db: Session = Depends(database.get_db)):
     try:
-        sahayak = models.Sahayak(phone_number=phone_number, email=email, first_name=first_name, last_name=last_name, aadhaar_number=aadhaar_number,
-                                 village=village, block_name=block_name, district=district, state=state, gender_category=gender_category,
-                                 education_level=education_level, is_active=is_active, is_approved=is_approved, pincode=pincode)
-        # sahayak = models.Sahayak(phone_number=phone_number, email=email, first_name=first_name)
-        db.add(sahayak)
-        print("Going to print Sahayak object:", sahayak)
+        db_sahayak = models.Sahayak(**sahayak.dict())
+        db.add(db_sahayak)
         db.commit()
-        db.refresh(sahayak)
-        return {"id": sahayak.id, "phone_number": sahayak.phone_number, "email": sahayak.email, "first_name": sahayak.first_name, "last_name": sahayak.last_name, "aadhaar_number": sahayak.aadhaar_number, "village": sahayak.village, "block_name": sahayak.block_name, "district": sahayak.district, "state": sahayak.state, "gender_category": sahayak.gender_category, "education_level": sahayak.education_level, "is_active": sahayak.is_active, "is_approved": sahayak.is_approved, "pincode": sahayak.pincode}
-        # return {"id": sahayak.id, "phone_number": sahayak.phone_number, "email": sahayak.email, "first_name": sahayak.first_name}
+        db.refresh(db_sahayak)
+        return {"id": db_sahayak.id, **sahayak.dict()}
     except Exception as e:
-        print("❌ Error occurred while adding Sahayak:", e) 
+        print("❌ Error occurred while adding Sahayak:", e)
+        return {"error": "Failed to add Sahayak"}
+
 
     
