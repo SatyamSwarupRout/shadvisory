74c59190f7390f5008da87373fda0865ecb8fbe4
Sukanta Nanda <sukantan@addyaa.com>
2025-10-18 15:16:59 +0530

    Update curl command for register_sahayak endpoint

--- sha_docker/test/add_sahayak.curl.sh
+++ sha_docker/test/add_sahayak.curl.sh
@@ -1,5 +1,18 @@
-curl -X POST "http://localhost:8000/register_sahayak" -H "Content-Type: application/json" -d '{
-     phone_number: "9876543210",
-     email: "sahayak_9876543210@example.com",
-     first_name: "John"
+curl -X POST "http://localhost:8000/register_sahayak" \
+     -H "Content-Type: application/json" \
+     -d '{
+     "phone_number": "55555555555",
+     "email": "",
+     "first_name": "",
+     "last_name": "",
+     "aadhaar_number": "",
+     "village": "",
+     "block_name": "",
+     "district": "",
+     "state": "",
+     "gender_category": "",
+     "education_level": "",
+     "is_active": true,
+     "is_approved": false,
+     "pincode": 123456
 }'
