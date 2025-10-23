27c56e80e3cabe6bac3f15266cc8ea9bd67f0e94
Sukanta Nanda <sukantan@addyaa.com>
2025-10-23 20:15:03 +0530

    optional fields in sahayakBase

--- sha_docker/app/pydantic_classes.py
+++ sha_docker/app/pydantic_classes.py
@@ -1,21 +1,22 @@
 from pydantic import BaseModel
 from typing import List, Optional
 
+
 class SahayakBase(BaseModel):
     phone_number: str
-    email: str
-    first_name: str
-    last_name: str
-    aadhaar_number: str
-    village: str
-    block_name: str
-    district: str
-    state: str
-    pincode: int
-    gender_category: str
-    education_level: str
-    is_active: bool
-    is_approved: bool
+    email: Optional[str] = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    aadhaar_number: Optional[str] = None
+    village: Optional[str] = None
+    block_name: Optional[str] = None
+    district: Optional[str] = None
+    state: Optional[str] = None
+    pincode: Optional[int] = None
+    gender_category: Optional[str] = None
+    education_level: Optional[str] = None
+    is_active: bool = True
+    is_approved: bool = False
 
 class SahayakCreate(BaseModel):
     phone_number: str
