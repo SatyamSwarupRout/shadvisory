1d6bee5be5055df6b38b3dc52b23e49403cc6cec
Sukanta Nanda <sukantan@addyaa.com>
2025-10-27 21:04:08 +0530

    few fixes to build env

--- sha_docker/test/add_farmer.sh
+++ sha_docker/test/add_farmer.sh
@@ -1,9 +1,7 @@
 curl -X POST "http://localhost:8000/register_farmer" \
      -H "Content-Type: application/json" \
      -d '{
-     "phone_number": "123123123",
-     "sahayak_phone_number": "6666666",
-     "first_name": "roshan",
-     "pincode": "456789"
+     "phone_number": "918918981",
+     "sahayak_phone_number": "6666666"
 }'
 
